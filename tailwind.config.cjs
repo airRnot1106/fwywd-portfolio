997a893dc411b3798460d7a6631e6bd3b55d8524
airRnot1106 <yngw.rintaro@gmail.com>
2023-01-25 01:53:08 +0900

    tailwindcssの色の設定を追加

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,20 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'f-black': {
+          100: '#384359',
+          200: '#262c3a',
+        },
+        'f-green': {
+          100: '#c5eaea',
+          200: '#6bc2c3',
+          300: '#2bb9ba',
+          400: '#008c8d',
+        },
+      },
+    },
   },
   plugins: [],
 };
