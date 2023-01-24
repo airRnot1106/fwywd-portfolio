03acb9b991996e1706c66859f376b5b7c20be100
airRnot1106 <yngw.rintaro@gmail.com>
2023-01-25 01:22:19 +0900

    eslintにtailwindcss用の設定を追加

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,11 +4,21 @@ module.exports = {
     'next/core-web-vitals',
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:tailwindcss/recommended',
     'prettier',
   ],
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'tailwindcss'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     sourceType: 'module',
+  },
+  rules: {
+    /* tailwindcss */
+    'tailwindcss/no-custom-classname': [
+      'warn',
+      {
+        config: 'tailwind.config.cjs',
+      },
+    ],
   },
 };
