187c8d501f284c3abb3a59b6bc1834d82e17eea0
airRnot1106 <yngw.rintaro@gmail.com>
2023-01-26 16:51:13 +0900

    pageを更新
    Featureを追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 import { AppFooter } from '@/components/common/site/molecules/AppFooter';
 import { AppHeader } from '@/components/common/site/molecules/AppHeader';
 import { AppHero } from '@/components/common/site/molecules/AppHero';
@@ -36,27 +38,41 @@ export default function Home() {
         </AboutArticleList>
       </div>
       <div
-        id="values"
-        className="flex flex-col items-center justify-center py-14 px-5"
+        className="bg-bottom bg-no-repeat"
+        style={{ backgroundImage: 'url(/bg-future.png)' }}
       >
-        <ArticleTitle mainTitle="価値観" subTitle="VALUES" />
-        <ValuesArticleList>
-          <ValuesArticleCard imageSrc="/value1.png" titleStr="価値観01">
-            <p className="text-f-black-100">
-              テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+        <div
+          id="values"
+          className="flex flex-col items-center justify-center py-14 px-5"
+        >
+          <ArticleTitle mainTitle="価値観" subTitle="VALUES" />
+          <ValuesArticleList>
+            <ValuesArticleCard imageSrc="/value1.png" titleStr="価値観01">
+              <p className="text-f-black-100">
+                テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+              </p>
+            </ValuesArticleCard>
+            <ValuesArticleCard imageSrc="/value2.png" titleStr="価値観02">
+              <p className="text-f-black-100">
+                テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+              </p>
+            </ValuesArticleCard>
+            <ValuesArticleCard imageSrc="/value3.png" titleStr="価値観03">
+              <p className="text-f-black-100">
+                テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+              </p>
+            </ValuesArticleCard>
+          </ValuesArticleList>
+        </div>
+        <div id="future" className="flex justify-center space-x-5 py-14 px-5">
+          <div className="mt-3 w-1/3 break-words">
+            <ArticleTitle mainTitle="3年後にやりたいこと" subTitle="FUTURE" />
+            <p className="mt-5 leading-8 text-f-black-100">
+              テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
             </p>
-          </ValuesArticleCard>
-          <ValuesArticleCard imageSrc="/value2.png" titleStr="価値観02">
-            <p className="text-f-black-100">
-              テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
-            </p>
-          </ValuesArticleCard>
-          <ValuesArticleCard imageSrc="/value3.png" titleStr="価値観03">
-            <p className="text-f-black-100">
-              テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
-            </p>
-          </ValuesArticleCard>
-        </ValuesArticleList>
+          </div>
+          <Image src="/future.png" width="500" height="500" alt="" />
+        </div>
       </div>
       <AppFooter />
     </>
